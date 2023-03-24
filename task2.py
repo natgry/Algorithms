19411798a72fe47fd7fbd3969cd446bde1a5fd41
natgry <natgry@yandex.ru>
2023-03-24 20:54:30 +0300

    Updated

--- task2.py
+++ task2.py
@@ -19,11 +19,12 @@ def get_cons_str(input_data: list[str]) -> str:
         letter_max_found = list(expected.keys())[list(expected.values()).index(max(expected.values()))]
         return letter_max_found
 
+    str_to_check = None
     for i in range(0, 4):
         for j in range(0, len(input_data)):
             x = input_data[j][i]
             ...
-           # str_to_check += row[i]
+            str_to_check += x
         get_max(str_to_check)
     x = str_to_check
 
